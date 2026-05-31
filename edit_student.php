e50134d8f2799b260f61ec0153cea3f5591abf3c
Soul-enenenenen <khanh4112006@gmail.com>
2026-05-31 20:13:05 +0700

    Update edit_student.php
    added GPA and changed course to department

--- edit_student.php
+++ edit_student.php
@@ -21,11 +21,12 @@ if (isset($_GET['id'])) {
 if (isset($_POST['update'])) {
 
     $name = $_POST['name'];
-    $email = $_POST['email'];
-    $course = $_POST['course'];
+    $student id = $_POST['student_id'];
+    $department = $_POST['department'];
+	$gpa = $_POST['gpa'];
 
     $update_sql = "UPDATE students 
-                   SET name='$name', email='$email', course='$course' 
+                   SET name='$name', student id='$student_id', department='$department', gpa='$gpa'; 
                    WHERE id='$id'";
 
     if (mysqli_query($conn, $update_sql)) {
@@ -46,14 +47,17 @@ if (isset($_POST['update'])) {
 <h2>Edit Student Record</h2>
 
 <form method="POST">
+	<label>Student ID:</label><br>
+    <input type="text" name="student id" value="<?php echo $student['student_id']; ?>" required><br><br>
+
     <label>Name:</label><br>
     <input type="text" name="name" value="<?php echo $student['name']; ?>" required><br><br>
 
-    <label>Email:</label><br>
-    <input type="email" name="email" value="<?php echo $student['email']; ?>" required><br><br>
-
-    <label>Course:</label><br>
-    <input type="text" name="course" value="<?php echo $student['course']; ?>" required><br><br>
+    <label>Department:</label><br>
+    <input type="text" name="deparment" value="<?php echo $student['department']; ?>" required><br><br>
+	
+    <label>GPA:</label><br>
+	<input type="text" name="gpa" value="<?php echo $student['gpa']; ?>" required><br><br>
 
     <button type="submit" name="update">Update Student</button>
 </form>
@@ -61,63 +65,3 @@ if (isset($_POST['update'])) {
 </body>
 </html><?php
 include 'db.php';
-
-// Check if ID is provided
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-
-    // Fetch existing student data
-    $sql = "SELECT * FROM students WHERE id = '$id'";
-    $result = mysqli_query($conn, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        $student = mysqli_fetch_assoc($result);
-    } else {
-        echo "Student not found.";
-        exit();
-    }
-}
-
-// Update student record
-if (isset($_POST['update'])) {
-
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $course = $_POST['course'];
-
-    $update_sql = "UPDATE students 
-                   SET name='$name', email='$email', course='$course' 
-                   WHERE id='$id'";
-
-    if (mysqli_query($conn, $update_sql)) {
-        echo "Student record updated successfully.";
-    } else {
-        echo "Error updating record: " . mysqli_error($conn);
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Edit Student</title>
-</head>
-<body>
-
-<h2>Edit Student Record</h2>
-
-<form method="POST">
-    <label>Name:</label><br>
-    <input type="text" name="name" value="<?php echo $student['name']; ?>" required><br><br>
-
-    <label>Email:</label><br>
-    <input type="email" name="email" value="<?php echo $student['email']; ?>" required><br><br>
-
-    <label>Course:</label><br>
-    <input type="text" name="course" value="<?php echo $student['course']; ?>" required><br><br>
-
-    <button type="submit" name="update">Update Student</button>
-</form>
-
-</body>
-</html>
