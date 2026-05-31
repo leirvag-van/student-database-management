308565fcb25b3fcd5fea4ca95e3ef0c1a8e76b49
ridho-pseudo <abdullahridho21@gmail.com>
2026-05-31 22:09:33 +0800

    Update index.php (static to dynamic style)
    Im updating the index file from the static/hard coded style to dynamic style that uses the database.

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
+<?php
+include 'config.php';
+
+$stmt = $pdo->query("SELECT * FROM students");
+$students = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Database Management System</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <div class="container">
+
     <h1>Student Database Management System</h1>
 
     <p>Welcome to the Student Academic Portal.</p>
@@ -25,13 +32,17 @@
             <th>GPA</th>
         </tr>
 
+        <?php foreach ($students as $student): ?>
         <tr>
-            <td>1</td>
-            <td>412000001</td>
-            <td>Kevin</td>
-            <td>Computer Science</td>
+            <td><?= $student['student_id'] ?></td>
+            <td><?= $student['name'] ?></td>
+            <td><?= $student['department'] ?></td>
+            <td><?= $student['gpa'] ?></td>
         </tr>
+        <?php endforeach; ?>
+
     </table>
+
 </div>
 
 </body>
